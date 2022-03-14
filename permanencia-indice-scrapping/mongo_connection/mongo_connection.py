5a1b3ed882dc62897108acff691af79f50bbf4a4
Guilherme Valle <guilherme.valle@leanonsystems.com>
2022-03-14 19:33:21 -0300

    Adiciona implementaçao de mongodb em python para inserir os links e autores

---
+++ permanencia-indice-scrapping/mongo_connection/mongo_connection.py
@@ -0,0 +1,19 @@
+from pymongo import MongoClient
+
+class MongoConnection:
+    mongo_client = MongoClient()
+    db_permanencia = mongo_client.permanencia_indice
+    authors_collection = db_permanencia.authors
+    links_collection = db_permanencia.links
+
+    def insert_author(self, author):
+        result_obj = self.authors_collection.insert_one(author)
+        return result_obj.acknowledged
+
+    def get_author(self, author_name):
+        author = self.authors_collection.find_one({"name": author_name})
+        return author
+
+    def insert_link(self, link):
+        result_obj = self.links_collection.insert_one(link)
+        return result_obj.acknowledged
